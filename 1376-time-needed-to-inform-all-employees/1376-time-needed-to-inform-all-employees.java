037959fc50a76de7e62dad71cfc07e7f87936b97
Mohammad-Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-06-08 11:35:53 +0530

    Time: 249 ms (5.01%), Space: 57.6 MB (85.71%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static void calculateCosttill(int[] costtill,int node,int[] manager,int[] informTime)
+    {
+        int leafnode=node,cost=0;
+        while(manager[node]!=-1)
+        {
+            cost+=informTime[manager[node]];
+            node=manager[node];
+        }
+        costtill[leafnode]=cost;
+    }
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int[] costtill=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            if(informTime[i]==0)
+            {
+                Solution.calculateCosttill(costtill,i,manager,informTime);
+            }
+        }
+        int maxcost=costtill[0];
+        int maxnode=0;
+        for(int i=0;i<n;i++){
+            if(maxcost<costtill[i])
+            {
+                maxcost=costtill[i];
+                maxnode=i;
+            }
+        }
+        int ans=0;
+        while(manager[maxnode]!=-1)
+        {
+            ans+=informTime[manager[maxnode]];
+            maxnode=manager[maxnode];
+        }
+        return ans;
+    }
+}
