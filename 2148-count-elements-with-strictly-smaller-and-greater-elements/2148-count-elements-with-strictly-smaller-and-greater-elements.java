4f3d5c5ebd466b17e8277e170250bf8b3a9c2e6b
Mohammad-Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-06-09 09:23:28 +0530

    Time: 1 ms (68.24%), Space: 41.2 MB (16.94%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -3,28 +3,9 @@ class Solution {
         Arrays.sort(nums);
         int size=nums.length;
         int count=0;
-        boolean leftok,rightok;
         for(int i=1;i<size-1;i++)
         {
-            leftok=false;
-            rightok=false;
-            for(int j=i-1;j>=0;j--)
-            {
-                if(nums[i]>nums[j])
-                {
-                    leftok=true;
-                    break;
-                }
-            }
-            for(int j=i+1;j<size;j++)
-            {
-                if(nums[i]<nums[j])
-                {
-                    rightok=true;
-                    break;
-                }
-            }
-            if(leftok && rightok)
+            if(nums[i]!=nums[0] && nums[i]!=nums[size-1])
             {
                 count++;
             }
