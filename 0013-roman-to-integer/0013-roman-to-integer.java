c7fdcd0d44a367ce2db91a62fa3ead383de46d68
Mohammad-Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-06-15 09:37:51 +0530

    Time: 4 ms (94.80%), Space: 43.7 MB (34.60%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,56 +1,82 @@
 import java.util.*;
-class Solution {
-    public static int getNum(String s,int i)
-    {
-        int n=0;
-        if(s.charAt(i)=='I')
-        {
-            n=1;
-        }
-        else if(s.charAt(i)=='V')
-        {
-            n=5;
-        }
-        else if(s.charAt(i)=='X')
-        {
-            n=10;
-        }
-        else if(s.charAt(i)=='L')
-        {
-            n=50;
-        }
-        else if(s.charAt(i)=='C')
-        {
-            n=100;
-        }
-        else if(s.charAt(i)=='D')
-        {
-            n=500;
-        }
-        else if(s.charAt(i)=='M')
-        {
-            n=1000;
-        }
-        return n;
-    }
-    public static int romanToInt(String s) {
-        int sum=0;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            int n1=getNum(s,i);
-            int n2=0;
-            if(i!=0)
-                n2=getNum(s,i-1);
-            if(n1>n2)
-            {
-                sum+=(n1-n2);
-                i--;
+// class Solution {
+//     public static int getNum(String s,int i)
+//     {
+//         int n=0;
+//         if(s.charAt(i)=='I')
+//         {
+//             n=1;
+//         }
+//         else if(s.charAt(i)=='V')
+//         {
+//             n=5;
+//         }
+//         else if(s.charAt(i)=='X')
+//         {
+//             n=10;
+//         }
+//         else if(s.charAt(i)=='L')
+//         {
+//             n=50;
+//         }
+//         else if(s.charAt(i)=='C')
+//         {
+//             n=100;
+//         }
+//         else if(s.charAt(i)=='D')
+//         {
+//             n=500;
+//         }
+//         else if(s.charAt(i)=='M')
+//         {
+//             n=1000;
+//         }
+//         return n;
+//     }
+//     public static int romanToInt(String s) {
+//         int sum=0;
+//         for(int i=s.length()-1;i>=0;i--)
+//         {
+//             int n1=getNum(s,i);
+//             int n2=0;
+//             if(i!=0)
+//                 n2=getNum(s,i-1);
+//             if(n1>n2)
+//             {
+//                 sum+=(n1-n2);
+//                 i--;
+//             }
+//             else
+//             {
+//                 sum+=n1;
+//             }
+//         }
+//         return sum;
+//     }
+// }
+class Solution{
+    public int romanToInt(String s) {
+    
+        int answer = 0, number = 0, prev = 0;
+
+        for (int j = s.length() - 1; j >= 0; j--) {
+            switch (s.charAt(j)) {
+                case 'M' -> number = 1000;
+                case 'D' -> number = 500;
+                case 'C' -> number = 100;
+                case 'L' -> number = 50;
+                case 'X' -> number = 10;
+                case 'V' -> number = 5;
+                case 'I' -> number = 1;
             }
-            else
-            {
-                sum+=n1;
+            if (number < prev) {
+                answer -= number;
             }
+            else {
+                answer += number;
+            }
+            prev = number;
         }
-        return sum;
+        return answer;
     }
 }
