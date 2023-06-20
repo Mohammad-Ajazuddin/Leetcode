c9982acc529253fa1af1afd81c7e60a272feb17b
Mohammad-Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-06-20 15:46:05 +0530

    Time: 12 ms (48.87%), Space: 60.9 MB (66.12%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,67 +1,68 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-    //     int size = nums.length;
-    //     int[] ans = new int[size];
-    //     int div = (k*2)+1;
-    //     if(size<div)
-    //     {
-    //         int i=0;
-    //         while(i<size)
-    //         {
-    //             ans[i]=-1;
-    //             i++;
-    //         }
-    //         return ans;
-    //     }
-    //     for(int i=0;i<size;i++)
-    //     {
-    //         if(i<=k)
-    //         {
-    //             if((i-k)<0)
-    //             {
-    //                 ans[i]=-1;
-    //             }
-    //             else{
-    //                 int count = k;
-    //                 long sum = 0L;
-    //                 int m=i-1;
-    //                 int n=i+1;
-    //                 while(count!=0 )
-    //                 {
-    //                     sum+=nums[m]+nums[n];
-    //                     m--;
-    //                     n++;
-    //                     count--;
-    //                 }
-    //                 sum+=nums[i];
-    //                 ans[i]=(int)sum/div;
-    //             }
-    //         }
-    //         if(i>k)
-    //         {
-    //             if((i+k)>size-1)
-    //             {
-    //                 ans[i]=-1;
-    //             }
-    //             else{
-    //                 int count = k;
-    //                 long sum = 0L;
-    //                 int m=i-1;
-    //                 int n=i+1;
-    //                 while(count!=0)
-    //                 {
-    //                     sum+=nums[m]+nums[n];
-    //                     m--;
-    //                     n++;
-    //                     count--;
-    //                 }
-    //                 sum+=nums[i];
-    //                 ans[i]=(int)sum/div;
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // } // one test case failed
+        // int size = nums.length;
+        // int[] ans = new int[size];
+        // int div = (k*2)+1;
+        // if(size<div)
+        // {
+        //     int i=0;
+        //     while(i<size)
+        //     {
+        //         ans[i]=-1;
+        //         i++;
+        //     }
+        //     return ans;
+        // }
+        // for(int i=0;i<size;i++)
+        // {
+        //     if(i<=k)
+        //     {
+        //         if((i-k)<0)
+        //         {
+        //             ans[i]=-1;
+        //         }
+        //         else{
+        //             int count = k;
+        //             long sum = 0L;
+        //             int m=i-1;
+        //             int n=i+1;
+        //             while(count!=0 )
+        //             {
+        //                 sum+=nums[m]+nums[n];
+        //                 m--;
+        //                 n++;
+        //                 count--;
+        //             }
+        //             sum+=nums[i];
+        //             ans[i]=(int)sum/div;
+        //         }
+        //     }
+        //     if(i>k)
+        //     {
+        //         if((i+k)>size-1)
+        //         {
+        //             ans[i]=-1;
+        //         }
+        //         else{
+        //             int count = k;
+        //             long sum = 0L;
+        //             int m=i-1;
+        //             int n=i+1;
+        //             while(count!=0)
+        //             {
+        //                 sum+=nums[m]+nums[n];
+        //                 m--;
+        //                 n++;
+        //                 count--;
+        //             }
+        //             sum+=nums[i];
+        //             ans[i]=(int)sum/div;
+        //         }
+        //     }
+        // }
+        // return ans;
+   // }one test case failed. This is also correct if ans[i]= (int) (sum/div)
+                                //parenthesis!! No checked it still 1 case failed
     int size = nums.length;
     int krange = (2*k) + 1;
     int[] ans = new int[size];
