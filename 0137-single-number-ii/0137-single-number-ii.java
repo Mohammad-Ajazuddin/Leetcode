29f5873832b9cc8e84fc0c6ef9c4fa0b1c8b3012
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-07-04 18:18:08 +0530

    Time: 6 ms (22.31%), Space: 44.1 MB (50.04%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int x:nums){
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)+1);
+            }
+            else{
+                map.put(x,1);
+            }
+            
+        }
+        int ans=0;
+        for(int key:map.keySet() ){
+            if(map.get(key)==1){
+                ans=key;
+            }
+        }
+        return ans;
+        //base cases
+          
+    }
+}
