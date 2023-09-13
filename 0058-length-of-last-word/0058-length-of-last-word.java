1beeee872c3f2c510e80420ccc5301abc59a881a
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-09-13 23:01:20 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (93.91%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,34 @@
+//My code
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String trimmed = s.trim();
+//         int totalLength = trimmed.length();
+//         int length = 0;
+//         for(int i=(totalLength-1);(i>=0 && trimmed.charAt(i)!=' ');i-- )
+//         {
+//             length++;
+//         }
+//         return length;
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        String trimmed = s.trim();
-        int totalLength = trimmed.length();
-        int length = 0;
-        for(int i=(totalLength-1);(i>=0 && trimmed.charAt(i)!=' ');i-- )
-        {
-            length++;
+        int i = s.length()-1;
+        int ans = 0;
+
+        while(i>=0) {
+            if(s.charAt(i)!=' ') {
+                ans++;
+            }
+            else {
+                if(ans>0) {
+                    return ans;
+                }
+            }
+            i--;
         }
-        return length;
+
+        return ans;
     }
 }
