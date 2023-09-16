19d17b5ca837cc12a5d8c3758ac1a14bea89b493
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-09-16 22:06:31 +0530

    Time: 12 ms (33.11%), Space: 44.3 MB (18.75%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         String s1 = s.trim(); //heading trailing spaces removed
+//         String[] words = s1.split("\\s+");// splits if one or more spaces like "a good    example"
+//         int words_count = words.length;
+//         StringBuilder result = new StringBuilder();
+//         for(int i=words_count - 1;i>=0;i--)
+//         {
+//             result.append(words[i]);   
+//             if(i!=0)
+//             {
+//                 result.append(" ");
+//             }
+//         }
+//         return result.toString();
+//     }
+// }
+
+
 class Solution {
     public String reverseWords(String s) {
-        String s1 = s.trim(); //heading trailing spaces removed
-        String[] words = s1.split("\\s+");// splits if one or more spaces like "a good    example"
+        //String s1 = s.trim(); //heading trailing spaces removed
+        String[] words = s.trim().split("\\s+");// splits if one or more spaces like "a good    example"
         int words_count = words.length;
-        StringBuilder result = new StringBuilder();
-        for(int i=words_count - 1;i>=0;i--)
+        String result="";
+        for(int i=words_count - 1;i>0;i--)
         {
-            result.append(words[i]);   
-            if(i!=0)
-            {
-                result.append(" ");
-            }
+            result += words[i] + " ";
         }
-        return result.toString();
+        result += words[0];
+        return result;
     }
 }
