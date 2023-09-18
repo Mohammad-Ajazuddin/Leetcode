41436320af929f3d283afbaf5362de2fca513f39
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-09-18 22:23:21 +0530

    Time: 7 ms (7.13%), Space: 44.4 MB (86.29%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] result = new int[k];
+        Map<Integer,Integer> rowSoldiers = new HashMap<>(m);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                //soldiers
+                if(mat[i][j]==1)
+                {
+                    if(rowSoldiers.containsKey(i))
+                    {
+                        rowSoldiers.put(i,rowSoldiers.get(i)+1);
+                    }
+                    else{
+                        rowSoldiers.put(i,1);
+                    }
+                }
+                else if(j==0)
+                {
+                    rowSoldiers.put(i,0);
+                }
+            }
+        }
+        
+        for(int i=0;i<k;i++)
+        {
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+            for(int j=0;j<m;j++)
+            {
+                if(min>rowSoldiers.get(j)){
+                    index = j;
+                    min = rowSoldiers.get(j);
+                }
+            }
+            rowSoldiers.put(index,Integer.MAX_VALUE);
+            result[i] = index;
+        }
+        return result;
+    }
+}
