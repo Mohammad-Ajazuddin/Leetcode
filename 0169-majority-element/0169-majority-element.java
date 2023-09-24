e060237128dcd401688f12d99133511a811eb7b1
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-09-24 11:43:12 +0530

    Time: 1 ms (99.86%), Space: 48.8 MB (19.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,62 @@
-//better approach   TC 0(NlogN) + O(N)
+//better approach   TC 0(NlogN) + O(N) SC = 0(N) if unique
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n = nums.length;
+//         HashMap<Integer,Integer> ele_count = new HashMap<>();
+//         //store in map
+//         for(int i=0;i<n;i++)
+//         {
+//             int value = ele_count.getOrDefault(nums[i],0);
+//             ele_count.put(nums[i],value+1);
+//         }
+//         //search in map
+//            //Interface  key-val pair       iterable (all k-v pairs)
+//         for(Map.Entry<Integer,Integer> it : ele_count.entrySet())
+//         {
+//             if(it.getValue()>(n/2))
+//                 return it.getKey();
+//         }
+//         return -1;
+//     }
+// } 10ms 46.3MB
+
+
+//Moores voting algo.
 class Solution {
     public int majorityElement(int[] nums) {
+        int count = 0;
+        int ele = 0;
         int n = nums.length;
-        HashMap<Integer,Integer> ele_count = new HashMap<>();
-        //store in map
+        //moores voting algo
+        for(int i =0;i<n;i++)
+        {
+            if(count == 0)
+            {
+                ele = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == ele)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        //verify the ele as majority or not 
+        //This part is actually not necessary as it is mentioned in q majority ele always exists
+        count = 0;
         for(int i=0;i<n;i++)
         {
-            int value = ele_count.getOrDefault(nums[i],0);
-            ele_count.put(nums[i],value+1);
+            if(ele==nums[i])
+            {
+                count++;
+            }
         }
-        //search in map
-           //Interface  key-val pair       iterable (all k-v pairs)
-        for(Map.Entry<Integer,Integer> it : ele_count.entrySet())
+        if(count>(n/2))
         {
-            if(it.getValue()>(n/2))
-                return it.getKey();
+            return ele;
         }
         return -1;
     }
