85d508534c602d03f1e90c4365f14a19aa3526aa
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-10-11 01:11:11 +0530

    Time: 2 ms (97.35%), Space: 46.5 MB (60.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -30,26 +30,90 @@
 // }
 
 //Better Approach -- using Hashing O(N*logN)
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         int n = nums.length;
+//         // least occurrence of the majority element:
+//         int minAppearance = (n/3) + 1;
+//         List<Integer> result = new ArrayList<>();
+//         HashMap<Integer,Integer> mpp = new HashMap<>();
+//         for(int i=0;i<n;i++)
+//         {
+//             int value = mpp.getOrDefault(nums[i],0);
+//             mpp.put(nums[i],value+1);
+            
+//             //checking if nums[i] is
+//             // the majority element:
+//             if(mpp.get(nums[i])==minAppearance)
+//             {
+//                 result.add(nums[i]);
+//             }
+            
+//             if(result.size()==2)
+//             {
+//                 break;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+//Optimal Moores Voting Algo
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int minAppearance = (n/3) + 1;
+        int count1 = 0;
+        int count2 = 0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
         List<Integer> result = new ArrayList<>();
-        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int n = nums.length;
         for(int i=0;i<n;i++)
         {
-            int value = mpp.getOrDefault(nums[i],0);
-            mpp.put(nums[i],value+1);
-            
-            if(mpp.get(nums[i])==minAppearance)
+            if(count1==0 && nums[i]!=ele2)
             {
-                result.add(nums[i]);
+                ele1 = nums[i];
+                count1++;
             }
-            
-            if(result.size()==2)
+            else if(count2==0 && nums[i]!=ele1)
             {
-                break;
+                ele2 = nums[i];
+                count2++;
             }
+            else if(ele1==nums[i])
+            {
+                count1++;
+            }
+            else if(ele2==nums[i])
+            {
+                count2++;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==ele1)
+            {
+                count1++;
+            }
+            else if(nums[i]==ele2)
+            {
+                count2++;
+            }
+        }
+        if(count1>(n/3))
+        {
+            result.add(ele1);
+        }
+        if(count2>(n/3))
+        {
+            result.add(ele2);
         }
         return result;
     }
