ec1bb6dcbe90a702f7807a7985292a00e6e5baea
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2023-10-20 21:53:38 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (98.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -27,33 +27,54 @@
 // }
 
 //DP to avoid computing of overlapping sub problems
-class Solution {
-    public static int countPaths(int i, int j, int[][] dp, int m, int n)
+// class Solution {
+//     public static int countPaths(int i, int j, int[][] dp, int m, int n)
+//     {
+//         if(i==(m-1) && j==(n-1))
+//         {
+//             return 1;
+//         }
+//         if(i>=m || j>=n)
+//         {
+//             return 0;
+//         }
+//         //check if a sub problem already computed. if so directly return it.
+//         if(dp[i][j]!=-1)
+//         {
+//             return dp[i][j];
+//         }
+//         else
+//         {
+//             return dp[i][j] = countPaths(i,j+1,dp,m,n) + countPaths(i+1,j,dp,m,n);
+//         }
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int i=0;i<m;i++)
+//         {
+//             Arrays.fill(dp[i],-1);    
+//         }
+//         return countPaths(0,0,dp,m,n);
+//     }
+// }
+
+//optimal -- using combinatorics
+
+class Solution{
+    public int uniquePaths(int m,int n)
     {
-        if(i==(m-1) && j==(n-1))
+        int N = m+n-2; // atmost right directions = n-1
+                    //lly down m-1. total m-1+n-1 = m+n-2
+        //total possible combinations
+        // totalC(r or d)
+        int r = m-1;
+        long result = 1;
+        //computing nCr
+        //Ex 10C3, r=3, 3 places 10*9*8/3*2*1
+        for(int i=1;i<=r;i++)
         {
-            return 1;
+            result = result * (N-r+i)/i;
         }
-        if(i>=m || j>=n)
-        {
-            return 0;
-        }
-        //check if a sub problem already computed. if so directly return it.
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        else
-        {
-            return dp[i][j] = countPaths(i,j+1,dp,m,n) + countPaths(i+1,j,dp,m,n);
-        }
-    }
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            Arrays.fill(dp[i],-1);    
-        }
-        return countPaths(0,0,dp,m,n);
+        return (int)result;
     }
 }
