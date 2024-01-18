dd2e5f6c6037e9ccf8c6375682dedc0278f6f831
Mohammad Ajazuddin <70790455+Mohammad-Ajazuddin@users.noreply.github.com>
2024-01-18 19:27:01 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (23.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,17 @@
+//recursion
+// class Solution {
+//     public int climbStairs(int n)
+//     {
+//         if(n<=2)
+//         {
+//             return n;
+//         }
+        
+//         return climbStairs(n-1)+climbStairs(n-2);
+//     }
+// } TLE
+
+//DP
 class Solution {
     public int climbStairs(int n) {
         if (n <= 2) {
